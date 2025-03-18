54f4d6a782518f2984b925db0c14688ec500357b
Fayecodeur <fayecodeur99@gmail.com>
2025-03-18 14:57:25 +0000

    création du composant header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import reactLogo from "../../assets/react.svg";
+import styles from "./header.module.css";
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <img src={reactLogo} alt="Logo react js" height={50} width={50} />
+        <div>
+          <h1>TaskFlow</h1>
+          <div className="">
+            <code className="color-gray">
+              Eliminer le choas, suiver le flux.
+            </code>
+          </div>
+        </div>
+      </div>
+      <code className="color-primary">v.1.0</code>
+    </div>
+  );
+}
