fe380c91e060c6a438b8ac20c1c5c795c064867b
Fayecodeur <fayecodeur99@gmail.com>
2025-03-18 16:15:24 +0000

    creation du composant footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from "./footer.module.css";
+export default function Footer() {
+  return (
+    <footer>
+      <code className={styles.footer}>Tu a terminer x taches</code>
+    </footer>
+  );
+}
