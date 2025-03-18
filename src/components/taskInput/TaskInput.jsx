63ff7c555e1747ceb9f6f262905d07441fb60f60
Fayecodeur <fayecodeur99@gmail.com>
2025-03-18 15:13:42 +0000

    creation du composant input

--- src/components/taskInput/TaskInput.jsx
+++ src/components/taskInput/TaskInput.jsx
@@ -1,3 +1,18 @@
+import styles from "./taskInput.module.css";
 export default function TaskInput() {
-  return <div>TaskInput</div>;
+  return (
+    <div className={`box ${styles.element}`}>
+      <h2 className={styles.title}>Ajoute ta prochaine tache</h2>
+      <form className={styles.container} action="">
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Indiquer un titre de tache explicite"
+        />
+        <button type="submit" className="button-primary">
+          Ajouter
+        </button>
+      </form>
+    </div>
+  );
 }
