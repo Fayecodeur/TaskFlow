166e6d0177d9307112b3b7fd674e35a98e8650b0
Fayecodeur <fayecodeur99@gmail.com>
2025-03-18 16:17:44 +0000

    correction des fautes

--- src/components/taskInput/TaskInput.jsx
+++ src/components/taskInput/TaskInput.jsx
@@ -2,12 +2,12 @@ import styles from "./taskInput.module.css";
 export default function TaskInput() {
   return (
     <div className={`box ${styles.element}`}>
-      <h2 className={styles.title}>Ajoute ta prochaine tache</h2>
+      <h2 className={styles.title}>Ajoute ta prochaine tâche</h2>
       <form className={styles.container} action="">
         <input
           type="text"
           className={styles.input}
-          placeholder="Indiquer un titre de tache explicite"
+          placeholder="Indique un titre de tâche explicite"
         />
         <button type="submit" className="button-primary">
           Ajouter
