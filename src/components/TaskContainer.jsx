d96494152f5cf7f69d482a6a1a0683703d675d0d
Fayecodeur <fayecodeur99@gmail.com>
2025-03-18 16:16:52 +0000

    inclusion des composants

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,3 +1,15 @@
+import Footer from "./footer/Footer.jsx";
+import Header from "./header/Header.jsx";
+import TaskInput from "./taskInput/TaskInput.jsx";
+import TaskList from "./TaskList/TaskList.jsx";
+
 export default function TaskContainer() {
-  return <div>TaskContainer</div>;
+  return (
+    <main>
+      <Header />
+      <TaskInput />
+      <TaskList />
+      <Footer />
+    </main>
+  );
 }
