a5f20540e0cf365776b65c34a615de3695c30525
Fayecodeur <fayecodeur99@gmail.com>
2025-03-20 14:48:17 +0000

    affichage des taches en cours

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,38 @@
 import TaskItem from "../TaskItem/TaskItem.jsx";
 import styles from "./taskList.module.css";
-export default function TaskList() {
+
+export default function TaskList({
+  taskList,
+  editTask,
+  deleteTask,
+  uncompletedTask,
+}) {
+  if (taskList && taskList.length > 0) {
+    return (
+      <div className="box">
+        <h2 className={styles.title}>
+          {uncompletedTask > 0
+            ? `Il te reste ${uncompletedTask} tâches 🗂️`
+            : "Bravo, tu as tout terminé ! 🎉"}
+        </h2>
+        <ul className={styles.container}>
+          {taskList.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              editTask={editTask}
+              deleteTask={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
   return (
     <div className="box">
-      <h2 className={styles.title}>Il te reste X tâches</h2>
-      <ul className={styles.container}>
-        <TaskItem />
-      </ul>
+      <h2 className={styles.title}>
+        Rien à faire, profite de ton temps libre 😌🎉
+      </h2>
     </div>
   );
 }
