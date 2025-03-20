82e5df7220b19ca8d3244ff2e7581b6ade3f617b
Fayecodeur <fayecodeur99@gmail.com>
2025-03-20 14:47:09 +0000

    ajout de la logique pour ajouter une tache

--- src/components/taskInput/TaskInput.jsx
+++ src/components/taskInput/TaskInput.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import styles from "./taskInput.module.css";
-export default function TaskInput() {
+import toast from "react-hot-toast";
+export default function TaskInput({ addTask }) {
+  const [title, setTitle] = useState("");
+
+  const handleChangeInput = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (title.trim() === "") {
+      toast.error("Le titre de la tâche est obligatoire !");
+      return;
+    }
+    addTask(title);
+    toast.success("Nouvelle tâche ajoutée !");
+    setTitle("");
+  };
+
   return (
     <div className={`box ${styles.element}`}>
-      <h2 className={styles.title}>Ajoute ta prochaine tâche</h2>
-      <form className={styles.container} action="">
+      <h2 className={styles.title}>Ajoute une tâche 📝</h2>
+      <form onSubmit={handleAddTask} className={styles.container}>
         <input
+          onChange={handleChangeInput}
+          value={title}
           type="text"
           className={styles.input}
           placeholder="Indique un titre de tâche explicite"
         />
-        <button type="submit" className="button-primary">
+        <button
+          type="submit"
+          className="button-primary"
+          style={{ outline: "none" }}
+        >
           Ajouter
         </button>
       </form>
