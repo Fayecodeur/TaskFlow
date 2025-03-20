29c7174f6b321dd034a804495c32e100cf3ed887
Fayecodeur <fayecodeur99@gmail.com>
2025-03-20 14:45:18 +0000

    ajout des differrent methodes pour l'application

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import Footer from "./footer/Footer.jsx";
 import Header from "./header/Header.jsx";
 import TaskInput from "./taskInput/TaskInput.jsx";
 import TaskList from "./TaskList/TaskList.jsx";
-
 export default function TaskContainer() {
+  const [taskList, setTaskList] = useState([]);
+
+  const addTask = (title) => {
+    const newTask = {
+      id: taskList.length ? taskList[taskList.length - 1].id + 1 : 1,
+      title: title,
+      completed: false,
+    };
+    setTaskList([...taskList, newTask]);
+  };
+
+  const editTask = (id, completedValue) => {
+    setTaskList(
+      taskList.map((task) =>
+        task.id === id ? { ...task, completed: completedValue } : task
+      )
+    );
+  };
+
+  const deleteTask = (id) => {
+    setTaskList(taskList.filter((task) => task.id !== id));
+  };
+
+  const getTaskCount = () => {
+    const completedTasks = taskList.filter((task) => task.completed).length;
+    const uncompletedTask = taskList.length - completedTasks;
+    return { completedTasks, uncompletedTask };
+  };
+
+  const { completedTasks, uncompletedTask } = getTaskCount();
+
   return (
     <main>
       <Header />
-      <TaskInput />
-      <TaskList />
-      <Footer />
+      <TaskInput addTask={addTask} />
+      <TaskList
+        taskList={taskList}
+        editTask={editTask}
+        deleteTask={deleteTask}
+        uncompletedTask={uncompletedTask}
+      />
+      <Footer completedTasks={completedTasks} />
     </main>
   );
 }
